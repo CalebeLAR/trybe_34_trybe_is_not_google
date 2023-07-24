9b3ed27341a75919221be67ac81358224467a888
calebeLAR <ca6ele@gmail.com>
2023-07-24 15:54:28 -0400

    requisito 01

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,40 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
+        self.length = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        """deve retornoar o tamanho atual da lista
+        sem percorrer ela toda!"""
+        return self.length
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        """deve inserir um novo elemento no final da fila"""
+        self.queue.append(value)
+        self.length += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        """deve remover o elemento que está no inicio da fila"""
+
+        if self.length == 0:
+            # caso a fila esteja vazia retorna None
+            return None
+
+        self.length -= 1
+
+        # pop(0) e del tem a mesma complexidade
+        # pop(0) pode ser ligeiramente mais rápida que del
+        return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        """deve retornar o elemento que está na posição index passada
+        por parâmentro"""
+
+        if not isinstance(index, int):
+            raise IndexError("Índice Inválido ou Inexistente")
+
+        if index < 0 or index >= self.length:
+            raise IndexError("Índice Inválido ou Inexistente")
+
+        return self.queue[index]
