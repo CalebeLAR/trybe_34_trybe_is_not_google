56cc4cd3a99943acceb161f7fb182fc6e5719960
calebeLAR <ca6ele@gmail.com>
2023-07-29 15:51:30 -0400

    requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,25 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    """deve receber um caminho para um arquivo txt e retornar um array com
+    cada linha do texto, deve tratar erros ao abrir e ler o arquivo"""
+
+    # Verifica se o arquivo existe
+    if not os.path.exists(path_file):
+        # o método os.path.exists verifica se o caminho para o arquivo existe
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return []
+
+    # Verifica se o arquivo é no formato .txt
+    if not path_file.lower().endswith(".txt"):
+        # o método sys.stderr.write é similar ao print, porém é utilizado
+        # para informar saidas de erro ou alertas e imprime no terminal sem
+        # quebras de linha
+        sys.stderr.write("Formato inválido\n")
+        return []
+
+    with open(path_file, 'r') as file:
+        unformatedLines = file.read().split("\n")
+        return unformatedLines
