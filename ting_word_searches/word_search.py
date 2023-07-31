812374201dbd565c0bd85f911a0ff4129ce5fda7
calebeLAR <ca6ele@gmail.com>
2023-07-31 16:42:52 -0400

    requisito 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,34 @@
+def linhas_onde_tem_a_alavra(index, word, line):
+    lines = []
+    for word_line in line.split(" "):
+        if word.lower() == word_line.lower().replace(".", ""):
+            lines.append({"linha": index + 1})
+    return lines
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    """Recebe uma palavra a ser pesquisada e uma fila com relatorios de
+    arquivos. Retorna  uma lista com informações sobre os arquivos de onde a
+    palavra foi encontrada"""
+    arr = []
+
+    for report in instance.queue:
+        ocorrencias = []
+        for index, line in enumerate(report['linhas_do_arquivo']):
+            if linhas_onde_tem_a_alavra(index, word, line):
+                new_line = linhas_onde_tem_a_alavra(index, word, line)
+                ocorrencias.extend(new_line)
+
+            result = {
+                "palavra": word.lower(),
+                "arquivo": report['nome_do_arquivo'],
+                "ocorrencias": ocorrencias,
+                }
+
+        if result['ocorrencias']:
+            arr.append(result)
+
+    return arr
 
 
 def search_by_word(word, instance):
