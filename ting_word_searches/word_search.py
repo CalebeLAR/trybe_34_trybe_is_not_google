c5d6d56225e45b14b2a7615949d06ae4071791c2
calebeLAR <ca6ele@gmail.com>
2023-07-31 17:02:41 -0400

    requisito 08

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,8 +1,16 @@
-def linhas_onde_tem_a_alavra(index, word, line):
+def linhas_onde_tem_a_palavra(index, word, line):
     lines = []
     for word_line in line.split(" "):
         if word.lower() == word_line.lower().replace(".", ""):
             lines.append({"linha": index + 1})
+    return lines
+
+
+def linhas_e_conteudo_onde_tem_a_palavra(index, word, line):
+    lines = []
+    for word_line in line.split(" "):
+        if word.lower() == word_line.lower().replace(".", ""):
+            lines.append({"linha": index + 1, "conteudo": line})
     return lines
 
 
@@ -15,8 +23,8 @@ def exists_word(word, instance):
     for report in instance.queue:
         ocorrencias = []
         for index, line in enumerate(report['linhas_do_arquivo']):
-            if linhas_onde_tem_a_alavra(index, word, line):
-                new_line = linhas_onde_tem_a_alavra(index, word, line)
+            if linhas_onde_tem_a_palavra(index, word, line):
+                new_line = linhas_onde_tem_a_palavra(index, word, line)
                 ocorrencias.extend(new_line)
 
             result = {
@@ -32,4 +40,25 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    """Recebe uma palavra a ser pesquisada e uma fila com relatorios de
+    arquivos. Retorna uma lista com informações sobre os arquivos de onde a
+    palavra foi encontrada e o conteudo das linhas"""
+    arr = []
+
+    for report in instance.queue:
+        ocorrencias = []
+        for index, line in enumerate(report['linhas_do_arquivo']):
+            if linhas_e_conteudo_onde_tem_a_palavra(index, word, line):
+                new_line = linhas_e_conteudo_onde_tem_a_palavra(index, word, line)
+                ocorrencias.extend(new_line)
+
+            result = {
+                "palavra": word.lower(),
+                "arquivo": report['nome_do_arquivo'],
+                "ocorrencias": ocorrencias,
+                }
+
+        if result['ocorrencias']:
+            arr.append(result)
+
+    return arr
